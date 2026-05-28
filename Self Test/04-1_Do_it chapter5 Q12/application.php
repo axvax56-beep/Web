9beb35616741ddb9665469c6a004428f121fbbf9
axvax56 <axvax56@gmail.com>
2026-05-28 17:59:37 +0900

    Web 네 번째 수업

--- Self Test/04-1_Do_it chapter5 Q12/application.php
+++ Self Test/04-1_Do_it chapter5 Q12/application.php
@@ -14,10 +14,30 @@
     echo "<h4>지원분야</h4>";
 
     $field=$_POST['field'];
-    
+    $field_str = ",";
+
     $num= count($field);
     for($i=0; $i<$num; $i++){
         echo "$field[$i],";
+        $field_str .=$field[$i];
+
+        if($i != $num-1){
+            $field_str .=",";
+        }
     }
     echo "<h4>지원 동기</h4> <p>$reason</p>";
+
+    $db = mysqli_connect('localhost', 'axvax56', 'a1s2d3f4!', 'axvax56');
+
+    mysqli_query($db, 'set names utf8');
+
+    $sql ="INSERT INTO applic(r_name, r_phone, field, reason) VALUES('$r_name','$r_phone','$field_str','$reason')";
+    $result=mysqli_query($db, $sql);
+
+    if ($result){
+        echo"지원서가 접수되었습니다. <br>";
+    }else{
+        echo"지원서 접수에 실패했습니다. 다시 시도해주세요. <br>";
+    }
+    mysqli_close($db);
 ?>
--- Self Test/04_Do_it chapter5 Q11/reservation.php
+++ Self Test/04_Do_it chapter5 Q11/reservation.php
@@ -2,12 +2,27 @@
 
     header("Content-Type:text/html; charset=utf-8");
 
-    $r_name= $_GET['r_name'];
-    $r_phone= $_GET['r_phone'];
-    $r_email= $_GET['r_email']; 
+    $r_name= $_POST['r_name'];
+    $r_phone= $_POST['r_phone'];
+    $r_email= $_POST['r_email'];
+    $now= date('Y-m-d H:i:s');
 
     echo "<p> 이 름: $r_name </p>";
     echo "<p> 전 화: $r_phone </p>";
     echo "<p> 이메일: $r_email </p>";
+    echo "<p> 예약 날짜: $now </p>";
 
+    $db= mysqli_connect('localhost', 'axvax56', 'a1s2d3f4!', 'axvax56');
+
+    mysqli_query($db, 'set names utf8');
+
+    $sql ="INSERT INTO reservation(r_name, r_phone, r_email, date) VALUES('$r_name','$r_phone','$r_email', '$now')";
+    $result=mysqli_query($db, $sql);
+    if ($result){
+        echo"예약되었습니다. <br>";
+    }else{
+        echo"예약에 실패했습니다. 다시 시도해주세요. <br>";
+    }
+
+    mysqli_close($db);
 ?>
